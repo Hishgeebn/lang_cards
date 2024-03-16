3414811e1f4e4d3cbcc0229f5fb205ba0ca4f369
Hishgeebn <khishithug@gmail.com>
2024-03-16 21:55:40 +0800

    py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bottle import Bottle, template, request, redirect, static_file
 from bottle_sqlalchemy import SQLAlchemyPlugin
 from sqlalchemy import create_engine
-from models import Base, Language
+from sqlalchemy.orm import joinedload
+from models import Base, Language, Word
 import os
 from dotenv import load_dotenv
+from math import ceil
 
 load_dotenv()
 
@@ -23,16 +25,24 @@ def static(filename):
     return static_file(filename, root='./static')
 
 
-@app.get('/no')
-def index():
-    return template('views/noname.tpl')
-
-
 @app.get('/')
-def index(db):
+@app.get('/<page:int>')
+def index(db, page=1):
+    page_size = 30
+    start = (page - 1) * page_size
     langs = db.query(Language).all()
-    error = request.query.get('error')
-    return template('views/index.tpl', langs=langs, error=error)
+    
+    total_words = db.query(Word).filter(Word.language_id.isnot(None)).count()
+    total_pages = ceil(total_words / page_size)
+    words = (
+        db.query(Word)
+        .options(joinedload(Word.language))
+        .filter(Word.language_id.isnot(None))
+        .order_by(Word.id)
+        .slice(start, start + page_size)
+        .all()
+    )
+    return template('views/index.tpl', langs=langs, words=words, page=page, total_pages=total_pages)   
 
 
 @app.post('/add-lang')
@@ -43,6 +53,29 @@ def add_lang(db):
     db.add(new_lang)
     db.commit()
 
+    return redirect('/')
+
+@app.post('/add-word')
+def add_word(db):
+    lang_id = request.forms.get('lang_id')
+    word = request.forms.get('word')
+    translation = request.forms.get('translation')
+    
+    language = db.query(Language).filter_by(id=lang_id).first()
+
+    new_word = Word(w_desc=word, translation=translation, language=language)
+    db.add(new_word)
+    db.commit()
+
+    return redirect('/')
+
+
+@app.route('/word/<id:int>', methods=['DELETE'])
+def delete_word(db, id):
+    word = db.query(Word).get(id)
+    if word:
+        db.delete(word)
+        db.commit()
     return redirect('/')
 
 
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -8,3 +8,13 @@ class Language(Base):
     __tablename__ = "languages"
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+    words = relationship("Word", back_populates="language") 
+
+
+class Word(Base):
+    __tablename__ = "words"
+    id = Column(Integer, primary_key=True)
+    w_desc = Column(String, unique=True)
+    translation = Column(String)
+    language_id = Column(Integer, ForeignKey("languages.id")) 
+    language = relationship("Language", back_populates="words")
